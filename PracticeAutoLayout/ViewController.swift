f3183807b69e36cc6af1e6b2743eda7d841439b4
Kai Koyamatsu <kai_koyamatsu@voyagegroup.com>
2019-12-16 13:30:08 +0900

    Initial Commit

---
+++ PracticeAutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeAutoLayout
+//
+//  Created by 小谷松　海 on 2019/12/16.
+//  Copyright © 2019 Kai Koyamatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
