72c56043962ef22c9d7975455b7073877a808ef7
Kai Koyamatsu <kai_koyamatsu@voyagegroup.com>
2019-12-18 13:48:30 +0900

    add table view

--- PracticeAutoLayout/ViewController.swift
+++ PracticeAutoLayout/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let test: [Int] = {
+        var arr: [Int] = []
+        for i in 1...10 {
+            arr.append(i)
+        }
+
+        return arr
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return test.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = String(test[indexPath.row])
+        return cell
+    }
 }
 
